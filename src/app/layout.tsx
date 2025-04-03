d6cc40f1800da112d8d2dfee944d582c01c9b397
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-04-03 14:24:20 +0530

    Customize (#3)
    * feat: fumadocs customize
    
    * feat: improve design
    
    * fix: remove things again
    
    * fix: seperate into it's own theme
    
    * fix: cleanup themes
    
    * chore: add todo
    
    * feat: add sidebar color customization variables
    
    * feat: implement theme customization and update layout styles
    
    * fix: enforce sidebar background color with !important
    
    * feat: add @fumadocs/cli as a development dependency
    
    * fix: update shadow variables to use color-fd-border and apply shadow-primary to buttons
    
    * fix: enforce button background color and update box-shadow styles
    
    * fix: disable theme switch in layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <body className="flex flex-col min-h-screen">
         <RootProvider theme={{
           enabled: false,
-          forcedTheme: 'light'
+          forcedTheme: 'light',
         }}>
           <div className="texture" />
           {children}
